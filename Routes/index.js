640ec45da404563b0227c5a1a90b463104d5e520
Saaoz <bastie_nb1998@hotmail.fr>
2023-08-26 16:16:04 +0200

    [ADD] Ajout du crud pour la table DCE

--- Routes/index.js
+++ Routes/index.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const projetRoutes = require('./projetRoute');
 const lotRoutes = require('./lotRoute');
 const marcheRoutes = require('./marcheRoute')
+const dceRoutes = require('./dceRoute')
 // Importez d'autres routes si vous en avez
 
 // Utilisez les routes individuelles avec leur préfixe approprié
 router.use('/projets', projetRoutes);
 router.use('/lot', lotRoutes);
 router.use('/marche', marcheRoutes);
+router.use('/dce', dceRoutes);
 // Utilisez d'autres routes avec leur préfixe approprié si nécessaire
 
 module.exports = router;
